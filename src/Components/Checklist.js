502305a86d7e011a841972bb15350288cb5aed6e
Aaron David <ajdavid128@gmail.com>
2023-03-20 17:39:48 -0500

    added state and itemlist component

--- src/Components/Checklist.js
+++ src/Components/Checklist.js
@@ -1,9 +1,16 @@
-
+import { Button } from "semantic-ui-react";
+import { useState } from "react";
 
 function Checklist() {
+
+    const [checklistItem, setChecklistItem] = useState([])
+
     return (
         <div className="avoid-nav">
+            
             <h1>Checklist</h1>
+            <Button>Create New Item</Button>
+
         </div>
     )
 };
---
+++ src/Components/ItemList.js
@@ -0,0 +1,11 @@
+
+
+function ItemList() {
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+export default ItemList;
