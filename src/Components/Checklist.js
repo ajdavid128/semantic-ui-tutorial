2a472243ae04c537aa5af0d189a90306fd2a8d60
Aaron David <ajdavid128@gmail.com>
2023-03-20 18:00:15 -0500

    added useEffect and fetch request in prep for db.json file fetching

--- src/Components/Checklist.js
+++ src/Components/Checklist.js
@@ -1,11 +1,17 @@
 import { Button } from "semantic-ui-react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import ItemList from "./ItemList";
 
 function Checklist() {
 
     const [checklistItem, setChecklistItem] = useState([])
+
+    useEffect(() => {
+        fetch("") // use JSON server to fetch to db.json file
+        .then(res => res.json())
+        .then(data => setChecklistItem(data))
+    }, []);
 
     return (
         <div className="avoid-nav">
